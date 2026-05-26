d55916b04810aa95fb2c5380f8ea350f5674ac41
Kizuna Akari <49898006+LtCoinCidenCe@users.noreply.github.com>
2026-05-27 00:10:38 +0800

    one more field

--- app/components/personal/IconEditing.tsx
+++ app/components/personal/IconEditing.tsx
@@ -32,37 +32,35 @@ const IconEditing: React.FC = () => {
     ctx.drawImage(originalBitmap, position.x, position.y, useSize, useSize, 0, 0, CROP_SIZE, CROP_SIZE);
   }, [originalBitmap, useSize, position]); // useEffect
 
-  return <div className="my-5 mx-auto flex flex-col items-center w-sm lg:w-2xl space-y-3">
-    <div className="flex w-full gap-5 items-center">
-      <input className="w-24 h-24 rounded-full self-start text-black/0 bg-gray-300 bg-contain hover:border-red-500 hover:border-dashed hover:border-2"
-        type="file"
-        style={{
-          backgroundImage: iconBackgroundImageStyle ? `url(${iconBackgroundImageStyle})` : undefined
-        }}
-        accept="image/*"
-        onChange={async (e) => {
-          const file = e.target.files?.[0];
-          if (!file) return;
-          let dotbitmap;
-          try { dotbitmap = await createImageBitmap(file); }
-          catch (error) {
-            alert("无法加载图片，请确保文件是有效的图片格式");
-            return;
-          }
-          if (dotbitmap.width < 200 || dotbitmap.height < 200) {
-            alert("请上传至少200x200像素的图片");
-            return;
-          }
-          setOriginalBitmap(dotbitmap);
-          setPosition({ x: 0, y: 0 });
-          setUseSize(Math.min(dotbitmap.width, dotbitmap.height));
-          setIsDragging(false);
-        }}
-      />
+  return <div className="flex w-full gap-5 items-center my-5">
+    <input className="w-24 h-24 rounded-full self-start text-black/0 bg-gray-300 bg-contain hover:border-red-500 hover:border-dashed hover:border-2"
+      type="file"
+      style={{
+        backgroundImage: iconBackgroundImageStyle ? `url(${iconBackgroundImageStyle})` : undefined
+      }}
+      accept="image/*"
+      onChange={async (e) => {
+        const file = e.target.files?.[0];
+        if (!file) return;
+        let dotbitmap;
+        try { dotbitmap = await createImageBitmap(file); }
+        catch (error) {
+          alert("无法加载图片，请确保文件是有效的图片格式");
+          return;
+        }
+        if (dotbitmap.width < 200 || dotbitmap.height < 200) {
+          alert("请上传至少200x200像素的图片");
+          return;
+        }
+        setOriginalBitmap(dotbitmap);
+        setPosition({ x: 0, y: 0 });
+        setUseSize(Math.min(dotbitmap.width, dotbitmap.height));
+        setIsDragging(false);
+      }}
+    />
 
-      <button className="px-4 h-12 bg-gray-300 hover:bg-gray-200 rounded-lg"
-        onClick={() => setIsIconOpen(true)}>修改头像</button>
-    </div>
+    <button className="px-4 h-12 bg-gray-300 hover:bg-gray-200 rounded-lg"
+      onClick={() => setIsIconOpen(true)}>修改头像</button>
 
     {originalBitmap && // 黑幕
       <div className="fixed inset-0 z-35 bg-black/36 flex justify-center items-center"
